1fe1b96aef8243df5e4e189901e963373634a40a
Peter Holmberg <peter.hlmbrg@gmail.com>
2019-12-10 18:03:43 +0100

    adding the rest of the items

---
+++ src/data/items/trinkets.ts
@@ -0,0 +1,67 @@
+// TODD: calculate proc dps
+export const trinkets = () => {
+  return [
+    {
+      name: 'Badge of the Swarmguard',
+      Proc: 0,
+    },
+    {
+      name: "Blackhand's Breadth",
+      crit: 2,
+    },
+    {
+      name: 'Counterattack Lodestone',
+      attackPower: 22,
+      parry: 1,
+    },
+    {
+      name: 'Darkmoon Card: Maelstrom',
+      Proc: 8.23,
+    },
+    {
+      name: 'Diamond Flask',
+      Proc: 0,
+    },
+    {
+      name: 'Drake Fang Talisman',
+      hit: 2,
+      attackPower: 56,
+      dodge: 1,
+    },
+    {
+      name: 'Earthstrike',
+      Proc: 0,
+    },
+    {
+      name: 'Hand of Justice',
+      attackPower: 20,
+      Proc: 0,
+    },
+    {
+      name: 'Heart of Wyrmthalak',
+      Proc: 4.94,
+    },
+    {
+      name: 'Jom Gabbar',
+      attackPower: 119.1666667,
+    },
+    {
+      name: 'Kiss of the Spider',
+      crit: 1,
+      hit: 1,
+    },
+    {
+      name: 'Rune of the Guard Captain',
+      hit: 1,
+      attackPower: 20,
+    },
+    {
+      name: 'Seal of the Dawn',
+      attackPower: 81,
+    },
+    {
+      name: "Slayer's Crest",
+      attackPower: 150.6666667,
+    },
+  ];
+};
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+type Phase = 1 | 1.5 | 2 | 2.5 | 3 | 4 | 5 | 6;
+
 export enum ItemSlot {
   Head = 'head',
   Neck = 'neck',
@@ -12,14 +14,17 @@ export enum ItemSlot {
   Ring = 'ring',
   Trinket = 'trinket',
   MainHand = 'mainhand',
-  OneHand = 'offhand',
+  OneHand = 'onehand',
+  OffHand = 'offhand',
   TwoHand = 'twohand',
+  Ranged = 'ranged',
 }
 
 export enum WeaponType {
   Axe = 'axe',
   Bows = 'bows',
   Daggers = 'daggers',
+  Fist = 'fist',
   Crossbow = 'crossbow',
   Mace = 'mace',
   Guns = 'guns',
@@ -27,34 +32,53 @@ export enum WeaponType {
   PoleArm = 'polearm',
 }
 
+export enum ArmorType {
+  Cloth = 'cloth',
+  Leather = 'leather',
+  Mail = 'mail',
+  Plate = 'plate',
+}
+
 interface Resistance {
-  Arcane: number;
-  Fire: number;
-  Frost: number;
-  Nature: number;
-  Shadow: number;
+  arcane?: number;
+  fire?: number;
+  frost?: number;
+  nature?: number;
+  shadow?: number;
 }
 
 interface WeaponSkill {
   value: number;
-  WeaponType: WeaponType[];
+  weaponType: WeaponType[];
 }
 
 export interface Item {
-  Name: string;
-  ItemSlot: ItemSlot;
-  Armor?: number;
-  Strength?: number;
-  Agility?: number;
-  Stamina?: number;
-  Intellect?: number;
-  Spirit?: number;
-  Resistance?: Resistance;
-  Crit?: number;
-  Hit?: number;
-  Defense?: number;
-  Dodge?: number;
-  Parry?: number;
-  AttackPower?: number;
-  WeaponSkill?: WeaponSkill;
+  name: string;
+  itemSlot: ItemSlot;
+  armorType?: ArmorType;
+  wowHeadId?: number;
+  armor?: number;
+  strength?: number;
+  agility?: number;
+  stamina?: number;
+  intellect?: number;
+  spirit?: number;
+  resistance?: Resistance;
+  crit?: number;
+  hit?: number;
+  defense?: number;
+  dodge?: number;
+  parry?: number;
+  attackPower?: number;
+  weaponSkill?: WeaponSkill;
+  proc?: number;
+  phase?: Phase;
+}
+
+export interface Weapon extends Item {
+  weaponType: WeaponType;
+  dps: number;
+  maxHit: number;
+  minHit: number;
+  speed: number;
 }
